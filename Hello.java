82dd551e7ef86c95e3e2b33039525f8272a3a414
rSARITHA <rsaritha035@gmail.com>
2016-07-23 14:51:00 +0530

    Hello.java

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package Javaprogram;
+import java.util.*;
+public class Hello {
+	public static void main(String[] args)
+	{
+		String str1="Hello";
+		for(int i=0;i<=str1.length()-1;i++)
+		{
+			System.out.println(str1);
+		}
+	
+	}
+}
