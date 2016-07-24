1811549128fba89356095b12b4623951f8a46f90
rSARITHA <rsaritha035@gmail.com>
2016-07-24 11:20:50 +0530

    StrToInt.java

---
+++ StrToInt.java
@@ -0,0 +1,11 @@
+package Javaprogram;
+import java.util.*;
+public class StrToInt {
+public static void main(String[] args) {
+	String str1;
+	Scanner s=new Scanner(System.in);
+	str1=s.next();
+	int a=str1.length();
+	System.out.println("Integer number is: "+a);
+}
+}
