0f23bf824e720f19546a96dbe077c66b6752d0d9
rSARITHA <rsaritha035@gmail.com>
2016-08-11 04:29:05 -0700

    Sum1.java

---
+++ Sum1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Sum {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int num=s.nextInt();
+		int k=0;
+		for(int i=0;i<=num;i++)
+		{
+			k=k+i;
+		}
+		System.out.println(k);
+	}
+}
