5af6f80e394d614630ad16987332ec1c5aed9fc9
rSARITHA <rsaritha035@gmail.com>
2016-08-11 02:36:35 -0700

    PressQ.java

---
+++ PressQ.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class PressQ {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+        String  c= s.nextLine();
+        if(s.equals("Q"))
+        {
+         System.exit(0);
+        }
+	}
+}
+
