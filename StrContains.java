dbe62cae12f15c78512c196021708d487c04c04f
rSARITHA <rsaritha035@gmail.com>
2016-08-15 05:24:45 -0700

    StrContains.java

---
+++ StrContains.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class StrContains {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		if(str.contains("ar"))
+		{
+		System.out.println("String is:"+str);
+		}
+		else
+		{
+			System.out.println("String is:"+str);
+		}
+	}
+
+}
