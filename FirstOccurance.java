506b0b98207b62f81bb54f7a01a28af37bdf4400
rSARITHA <rsaritha035@gmail.com>
2016-08-31 09:34:37 +0530

    FirstOccurance.java

---
+++ FirstOccurance.java
@@ -0,0 +1,16 @@
+
+public class FirstOccurance {
+	public static void main(String[] args) {
+		String a[]={"AF","D","EF","G"};
+		for(int i=0;i<a.length;i++)
+		{
+            if(a[i].length()==1)		
+			{
+				System.out.println(a[i]);
+			
+			}
+		}
+
+	}
+
+}
